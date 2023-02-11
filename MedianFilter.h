bb18ff93f563e5e0f5567c1425a18131a4b12d14
p5ych0p41h <75533543+p5ych0p41h@users.noreply.github.com>
2023-02-11 11:38:08 +0300

    add MedianFiIter

---
+++ MedianFilter.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "Windows.h"
+
+using namespace System;
+using namespace System::Drawing;
+using namespace System::Windows::Forms;
+
+
+ref class MedianFilter {
+private:
+	Bitmap^ image;
+
+    byte** imageRed;
+    byte** imageGreen;
+    byte** imageBlue;
+
+    void init();
+
+public:
+    MedianFilter(Bitmap^);
+    void filtering(int);
+    Bitmap^ getImage();
+};
